2ca0f953a4a740243443c870c1d4bb97c0367d98
AcDel-off <145152125+AcDel-off@users.noreply.github.com>
2024-10-07 20:02:16 +0300

    added console feature

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,22 @@ namespace ConsoleApp1
         {
             bool isHidden = false;
             Console.WriteLine($"Hidden status: {isHidden}");
+
+            Console.WriteLine($"1. Change Hidden status\n2. Exit");
+            string choise = Console.ReadLine();
+
+            switch ( choise )
+            {
+                case "1":
+                    isHidden = !isHidden;
+                    Console.WriteLine($"Hidden status: {isHidden}");
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("Not right command");
+                    break;
+            }
         }
     }
 }
