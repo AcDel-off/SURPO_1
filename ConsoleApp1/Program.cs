b94ee49c486eaaacd51ff6d9146442ade48fb530
AcDel-off <145152125+AcDel-off@users.noreply.github.com>
2024-10-31 22:11:26 +0300

    enemy health added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,25 @@
 using System;
 
-namespace ConsoleApp1
+namespace Enemy
 {
     internal class Program
     {
+        static bool isHidden = false;
+        static int health = 100;
+
         static void Main(string[] args)
         {
-            bool isHidden = false;
+            HiddenStatus();
+            HealthLevel();
+        }
+
+        private static void HiddenStatus()
+        {
             Console.WriteLine($"Hidden status: {isHidden}\n");
 
             while (true)
             {
-                Console.WriteLine($"1. Change Hidden status\n2. Exit");
+                Console.WriteLine($"1. Change Hidden status\n2. Next step");
                 string choise = Console.ReadLine();
 
                 switch (choise)
@@ -19,6 +27,38 @@ namespace ConsoleApp1
                     case "1":
                         isHidden = !isHidden;
                         Console.WriteLine($"Hidden status: {isHidden}");
+                        break;
+                    case "2":
+                        return;
+                    default:
+                        Console.WriteLine("Not right command");
+                        break;
+                }
+            }
+        }
+
+        private static void HealthLevel()
+        {
+            Console.WriteLine($"Health level: {health}\n");
+
+            while (true)
+            {
+                Console.WriteLine($"1. Change health level\n2. Exit");
+                string choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1":
+                        try
+                        {
+                            health = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            break;
+                        }
+                        Console.WriteLine($"Health level: {health}");
                         break;
                     case "2":
                         return;
